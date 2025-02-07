3f345fc021ca236a52618b2f238f28ac1feff4b9
Legend286 <asagpol@gmail.com>
2025-02-07 13:35:12 +0000

    poopy fps counter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::sync::Mutex;
+use std::time::Instant;
+use QueryType::Timestamp;
 #[cfg(target_arch = "wasm32")]
 use wasm_bindgen::prelude::*;
 use winit::{
@@ -6,10 +9,29 @@ use winit::{
     window::WindowBuilder,
 };
 
-use wgpu::{Adapter, Backends, CompositeAlphaMode, Device, DeviceDescriptor, Features, Instance, InstanceDescriptor, InstanceFlags, Limits, PipelineCompilationOptions, PipelineLayoutDescriptor, PresentMode, Queue, StoreOp, Surface, SurfaceConfiguration, TextureFormat, TextureUsages, TextureViewDescriptor};
+use wgpu::{Adapter, Backends, Buffer, CommandEncoder, CompositeAlphaMode, Device, DeviceDescriptor, Features, Instance, InstanceDescriptor, InstanceFlags, Limits, MemoryHints, PipelineCompilationOptions, PipelineLayoutDescriptor, PresentMode, QuerySet, QueryType, Queue, RenderPassTimestampWrites, StoreOp, Surface, SurfaceConfiguration, TextureFormat, TextureUsages, TextureViewDescriptor};
 use wgpu::util::DeviceExt;
 use winit::dpi::{LogicalSize, PhysicalSize};
 use winit::event_loop::ControlFlow::Poll;
+
+struct GPUTimer<'a> {
+    encoder: &'a mut CommandEncoder,
+    query_set: &'a QuerySet,
+    query_index: u32,
+}
+
+impl<'a> GPUTimer<'a> {
+    fn start(encoder: &'a mut CommandEncoder, query_set: &'a QuerySet, query_index: u32) -> Self {
+        encoder.write_timestamp(query_set, query_index);
+        Self { encoder, query_set, query_index }
+    }
+}
+
+impl<'a> Drop for GPUTimer<'a> {
+    fn drop(&mut self) {
+        self.encoder.write_timestamp(self.query_set, self.query_index + 1);
+    }
+}
 
 #[repr(C)]
 #[derive(Clone, Copy, bytemuck::Pod, bytemuck::Zeroable)]
@@ -111,7 +133,7 @@ fn main() {
 
     let window_ref = Arc::clone(&window);
 
-    let (mut surface, mut device, mut queue, mut config) = pollster::block_on(run_wgpu(&window));
+    let (mut surface, mut device, mut queue, mut config, mut query) = pollster::block_on(run_wgpu(&window));
 
     // begin triangle
 
@@ -205,93 +227,143 @@ fn main() {
 
     // end shader
 
-    event_loop.run(move |event, event_loop|
-        {
-            event_loop.set_control_flow(Poll);
+    let query_set = device.create_query_set(&wgpu::QuerySetDescriptor {
+        label: Some("Timestamp Query Set"),
+        ty: wgpu::QueryType::Timestamp,
+        count: 2, // 2 queries: start and end timestamps
+    });
 
-            //  let old_window_size = (config.width, config.height);
-            let min_window_size = get_min_window_sizes();
+    let query_resolve_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+        label: Some("Query Resolve Buffer"),
+        size: std::mem::size_of::<u64>() as wgpu::BufferAddress * 2,
+        usage: wgpu::BufferUsages::COPY_SRC | wgpu::BufferUsages::QUERY_RESOLVE,
+        mapped_at_creation: false,
+    });
 
-            match event
-            {
-                Event::WindowEvent { event, .. } => match event
-                {
-                    WindowEvent::CloseRequested =>
-                        {
-                            event_loop.exit();
+    let read_buffer = Arc::new(device.create_buffer(&wgpu::BufferDescriptor {
+        label: Some("Read Buffer"),
+        size: std::mem::size_of::<u64>() as wgpu::BufferAddress * 2,
+        usage: wgpu::BufferUsages::MAP_READ | wgpu::BufferUsages::COPY_DST,
+        mapped_at_creation: false,
+    }));
+
+    let mut frame_time: Option<f32> = None; // Store the frame time
+
+
+    let window = Arc::new(Mutex::new(window_ref));  // Wrap the window in an Arc<Mutex>
+
+    event_loop.run(move |event, event_loop| {
+        match event {
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::RedrawRequested => {
+                    let frame = match surface.get_current_texture() {
+                        Ok(frame) => frame,
+                        Err(err) => {
+                            eprintln!("Dropped frame: {:?}", err);
                             return;
                         }
+                    };
 
-                    WindowEvent::Resized(new_size) =>
-                        {
-                            config.width = new_size.width.max(min_window_size.0);
-                            config.height = new_size.height.max(min_window_size.1);
-                            surface.configure(&device, &config);
-                            println!("Resized to {:?}x{:?}", config.width, config.height);
-                        }
+                    let view = frame.texture.create_view(&TextureViewDescriptor::default());
+                    let clear_color = wgpu::Color { r: 0.5, g: 0.8, b: 0.2, a: 1.0 };
 
-                    WindowEvent::RedrawRequested =>
-                        {
-                            let frame = match surface.get_current_texture()
-                            {
-                                Ok(frame) => frame,
-                                Err(err) =>
-                                    {
-                                        eprintln!("Dropped frame: {:?}", err);
-                                        return;
-                                    }
-                            };
+                    let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: Some("Render Encoder") });
 
-                            let view = frame.texture.create_view(&TextureViewDescriptor::default());
+                    // Start the GPU timer
+                    {
+                        let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                            label: Some("Render Pass"),
+                            color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                                view: &view,
+                                resolve_target: None,
+                                ops: wgpu::Operations {
+                                    load: wgpu::LoadOp::Clear(clear_color),
+                                    store: StoreOp::Store,
+                                },
+                            })],
+                            timestamp_writes: Some(wgpu::RenderPassTimestampWrites {
+                                query_set: &query_set,
+                                beginning_of_pass_write_index: Some(0), // Start timestamp
+                                end_of_pass_write_index: Some(1),
+                            }),
+                            depth_stencil_attachment: None,
+                            occlusion_query_set: None,
+                        });
 
-                            let clear_color = wgpu::Color
-                            {
-                                r: 0.5,
-                                g: 0.8,
-                                b: 0.2,
-                                a: 1.0,
-                            };
+                        render_pass.set_pipeline(&render_pipeline);
+                        render_pass.set_vertex_buffer(0, vertex_buffer.slice(..));
+                        render_pass.draw(0..3, 0..1);
+                    }
 
-                            let mut encoder = device.create_command_encoder(
-                                &wgpu::CommandEncoderDescriptor { label: Some("Render Encoder") });
-                            {
-                                let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor
-                                {
-                                    label: Some("Render Pass"),
-                                    timestamp_writes: None,
-                                    occlusion_query_set: None,
-                                    color_attachments: &[Some(wgpu::RenderPassColorAttachment
-                                    {
-                                        view: &view,
-                                        resolve_target: None,
-                                        ops: wgpu::Operations
-                                        {
-                                            load: wgpu::LoadOp::Clear(clear_color),
-                                            store: StoreOp::Store,
-                                        },
-                                    })],
-                                    depth_stencil_attachment: None,
-                                });
+                    // Resolve query results into the query_resolve_buffer
+                    encoder.resolve_query_set(&query_set, 0..2, &query_resolve_buffer, 0);
+                    // Copy query_resolve_buffer into the read_buffer for reading
+                    encoder.copy_buffer_to_buffer(&query_resolve_buffer, 0, &read_buffer, 0, query_resolve_buffer.size());
 
-                                render_pass.set_pipeline(&render_pipeline);
-                                render_pass.set_vertex_buffer(0, vertex_buffer.slice(..));
-                                render_pass.draw(0..3, 0..1);
+                    // Submit the encoder commands to the queue
+                    queue.submit(Some(encoder.finish()));
 
+                    // Map and process buffer data asynchronously
+                    read_buffer.slice(..).map_async(wgpu::MapMode::Read, {
+                        let read_buffer_clone = Arc::clone(&read_buffer);
+                        let window_clone = Arc::clone(&window); // Clone the window reference
+
+                        move |result| {
+                            if let Ok(()) = result {
+                                let slice = read_buffer_clone.slice(..); // Borrow the slice inside the closure
+                                let data = slice.get_mapped_range();
+                                let start_timestamp = bytemuck::from_bytes::<u64>(&data[0..8]);
+                                let end_timestamp = bytemuck::from_bytes::<u64>(&data[8..16]);
+                                let frame_time_calc = ((end_timestamp - start_timestamp) as f32) * 1e-6;
+
+                                // Store the frame time in the variable
+                                frame_time = Some(frame_time_calc);
+
+                                // Release the mapped memory
+                                drop(data);
+
+                                // Unmap the buffer explicitly after reading
+                                read_buffer_clone.unmap();
+
+                                // Update the window title with the frame time if available
+                                window_clone.lock().unwrap().set_title(&format!(
+                                        "{title} - FPS: {fps} - Frame time: {:.2} ms",
+                                        frame_time.unwrap_or(0.0),
+                                        fps = 1000.0 / frame_time.unwrap_or(1.0),
+                                        title = get_window_title()));
+
+
+                            } else {
+                                eprintln!("Failed to map buffer for reading.");
                             }
-
-                            queue.submit(Some(encoder.finish()));
-
-                            frame.present();
-                            window_ref.request_redraw();
                         }
-                    _ => {}
-                },
+                    });
+
+                    // Keep the GPU active after submission during rendering
+                    device.poll(wgpu::Maintain::Wait);
+                    frame.present();
+                }
+
+                // Handle the window close event
+                WindowEvent::CloseRequested => {
+                    event_loop.exit(); // Stops the event loop, closing the window
+                }
+
+                // Handle window resizing
+                WindowEvent::Resized(physical_size) => {
+                    surface.configure(&device, &config);
+                }
+
                 _ => {}
-            }
-        }).expect("Error during initialisation.");
+            },
+            _ => {}
+        }
+    }).expect("Error during initialization.");
+
+
 }
 
-async fn run_wgpu(window: &winit::window::Window, ) -> (Surface, Device, Queue, SurfaceConfiguration)
+async fn run_wgpu(window: &winit::window::Window, ) -> (Surface, Device, Queue, SurfaceConfiguration, QuerySet)
 {
     let backend = get_backend();
     println!("Using backend: {}", get_backend_name(backend));
@@ -319,11 +391,22 @@ async fn run_wgpu(window: &winit::window::Window, ) -> (Surface, Device, Queue, 
 
     println!("Adapter requested successfully!");
 
-    let (device, queue) = adapter
+    let required_features = if(adapter.features().contains(Features::TIMESTAMP_QUERY))
+    {
+        println!("Timestamp Query Enabled!");
+        Features::TIMESTAMP_QUERY
+    }
+    else
+    {
+        println!("Timestamp Query Disabled!");
+        Features::empty()
+    };
+
+let (device, queue) = adapter
         .request_device(
             &DeviceDescriptor
             {
-                required_features: Features::empty(),
+                required_features: required_features,
                 required_limits: if cfg!(target_arch = "wasm32") {
                     Limits::downlevel_webgl2_defaults()
                 }
@@ -331,8 +414,8 @@ async fn run_wgpu(window: &winit::window::Window, ) -> (Surface, Device, Queue, 
                 {
                     Limits::default()
                 },
-                label: None,
-                memory_hints: Default::default(),
+                label: Some("Adapter"),
+                memory_hints: MemoryHints::Performance,
             },
             None, // Trace path
         )
@@ -340,6 +423,14 @@ async fn run_wgpu(window: &winit::window::Window, ) -> (Surface, Device, Queue, 
         .expect("Failed to create device!");
 
     println!("Device and queue initialized!");
+
+    let supports_timestamp_query = device.features().contains(Features::TIMESTAMP_QUERY);
+
+    let query_set = device.create_query_set(&wgpu::QuerySetDescriptor {
+        label: Some("Timestamp Queries"),
+        ty: QueryType::Timestamp,
+        count: 2, // We need two timestamps: one at the start and one at the end of the render pass.
+    });
 
     let swap_chain_format = get_swap_chain_format();
 
@@ -361,5 +452,5 @@ async fn run_wgpu(window: &winit::window::Window, ) -> (Surface, Device, Queue, 
     surface.configure(&device, &config);
     println!("Swap chain configured! {}", format!("{:?}", config));
 
-    (surface, device, queue, config)
+    (surface, device, queue, config, query_set)
 }
